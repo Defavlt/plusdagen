db50c20e2bcd8f27b033987125afd7800d5436f2
unknown <ashkan.zahiraldini@PLUS-R9P8F94.learnet.se>
2012-11-15 11:54:47 +0100

    lagt till plats.php, schema.php, vad.php

---
+++ src/php/content/schema.php
@@ -0,0 +1,53 @@
+﻿	
+		<div id="mainbox"> <!-- 3 kolumnerna ligger här -->
+			<div id="breadcrumb">
+			<a href="index.html">Hem</a> > <a href="massa.html">Mässa</a> > Schema
+			</div>
+			<div id="vad">
+			<h1>Schema för arbetsmarknadsdagen</h1>
+				<table border="6" width="500">
+				<tr>
+				<th>Företag</th>
+				<th>Seminarie</th>
+				<th>Dag</th>
+				<th>Tid</th>
+				</tr>
+
+				<tr>
+				<td>Företag 1</td>
+				<td>Affärside</td>
+				<td>24/5-2013</td>
+				<td>kl:9.00</td>
+				</tr>
+
+				<tr>
+				<td>Företag 2</td>
+				<td>Varför jobba inom it?</td>
+				<td>24/5-2013</td>
+				<td>kl:11.00</td>
+				</tr>
+
+				<tr>
+				<td>Företag 3</td>
+				<td>All tid i världen</td>
+				<td>24/5-2013</td>
+				<td>kl:13.00</td>
+				</tr>
+
+				<tr>
+				<td>Företag 4</td>
+				<td>Framtiden</td>
+				<td>24/5-2013</td>
+				<td>kl.15.00</td>
+				</tr>
+
+				<tr>
+				<td>Företag 5</td>
+				<td>Din tid</td>
+				<td>24/5-2013</td>
+				<td>kl.17.00</td>
+				</tr>
+				</table>
+
+			</div>
+	</div> <!-- mainbox slut -->
---
+++ src/php/content/vad.php
@@ -0,0 +1,15 @@
+﻿		<div id="mainbox"> <!-- 3 kolumnerna ligger här -->
+			<div id="breadcrumb">
+			<a href="index.html">Hem</a> > <a href="massa.html">Mässa</a> > Vad/Varför?
+			</div>
+			<div id="vad">
+			
+			<h1>Vad är en arbetsmarknadsdag?</h1>
+			<p>
+			Det här är ett sätt för företag och Elever att komma i kontakt  och dela erfarenheter med varandra.  Arbetsmarknadsdagens  största mål är att eleverna ska få insikt i det framtida arbetslivet. Det är också ett perfekt sett för företagen att marknadsföra sig själva. 
+			På detta sett kan du som elev träffa både nya och etablerade företag i vår region. 
+			Och vem vet kanske framtida arbetsgivare.
+			Alla är välkomna att utvecklas med oss!
+			</p>
+			</div>
+		</div> <!-- mainbox -->
