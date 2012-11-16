9596652723abe232ab1388ece6eb170ebfe3a772
Christopher Holmberg <christopherholmberg@live.se>
2012-11-16 12:18:39 +0100

    Footer och css

--- src/php/modules/footer.php
+++ src/php/modules/footer.php
@@ -1,23 +1,31 @@
 <div id="footer">
-	<div id="footer_container">
-		<div id="footer_left">
-		Mässa:
-		<ul>
-		<li><a href="#">Hem</a></li>
-		<li><a href="#">Vad/Varför?</a></li>
-		<li><a href="#">Schema</a></li>
-		<li><a href="#">Plats</a></li>
-		</ul>
+	<div id="foot">
+		<div id="left">
+			<p class="text1">Mässa</p>
+			<p>Adress: Kalmarrondellen</p>
+			<p>Adress: Kalmarrondellen</p>
+			<p>Adress: Kalmarrondellen</p>
 		</div>
-		
-		<div id="footer_right">
-		Adress:
-		Teknikhögskolan
-		Malmvägen 7
-		39232 Kalmar
-		Mail: mail@mail.com
+		<div id="center">
+			<p class="text1">Registrera</p>
+			<p>Adress: Kalmarrondellen</p>
+			<p>Adress: Kalmarrondellen</p>
+			<p>Adress: Kalmarrondellen</p>
+		</div>
+		<div id="right">
+			<p class="text1">Logga in</p>
+			<p>Adress: Kalmarrondellen</p>
+			<p>Adress: Kalmarrondellen</p>
+			<p>Adress: Kalmarrondellen</p>
+		</div>
+		<div id="right2">
+			<p class="text1">Nyheter</p>
+			<p>Adress: Kalmarrondellen</p>
+			<p>Adress: Kalmarrondellen</p>
+			<p>Adress: Kalmarrondellen</p>
 		</div>
 	</div>
+</div>
 </div>
 </body>
 </html>
