7878b148ee51ecdb1b3dc1341bc7a7a30399938d
Christopher Holmberg <christopherholmberg@live.se>
2012-11-20 14:33:58 +0100

    Nya andraingar

--- src/php/modules/head.php
+++ src/php/modules/head.php
@@ -5,54 +5,22 @@
 <link href="css/font.css" rel="stylesheet" type="text/css"/>
 <link href="css/slider.css" rel="stylesheet" type="text/css"/>
 <link href="css/meny.css" rel="stylesheet" type="text/css"/>
-<link href="css/footer.css" rel="stylesheet" type="text/css"/>
 <link href="css/pos_struktur.css" rel="stylesheet" type="text/css"/>
 
 <title>Arbetsmarknadsdagen</title>
 
+    <!-- bjqs.css contains the *essential* css needed for the slider to work, den här behövs annars så chrasr slidern -->
+    <link rel="stylesheet" href="css/bjqs.css">
 
-    <!-- demo.css contains additional styles used to set up this demo page - not required for the slider --> 
+
+    <!-- demo.css contains additional styles used to set up this demo page - not required for the slider -->
+    <link rel="stylesheet" href="css/slider.css">
 
     <!-- load jQuery and the plugin -->
-
-    
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.5.1/jquery.min.js"></script>
-	<script src="js/slides.min.jquery.js"></script>
-	<script>
-		$(function(){
-			$('#slides').slides({
-				preload: true,
-				preloadImage: 'img/loading.gif',
-				play: 5000,
-				pause: 2500,
-				hoverPause: true,
-				animationStart: function(current){
-					$('.caption').animate({
-						bottom:-35
-					},100);
-					if (window.console && console.log) {
-						// example return of current slide number
-						console.log('animationStart on slide: ', current);
-					};
-				},
-				animationComplete: function(current){
-					$('.caption').animate({
-						bottom:0
-					},200);
-					if (window.console && console.log) {
-						// example return of current slide number
-						console.log('animationComplete on slide: ', current);
-					};
-				},
-				slidesLoaded: function() {
-					$('.caption').animate({
-						bottom:0
-					},200);
-				}
-			});
-		});
-	</script>
+    <script src="http://code.jquery.com/jquery-1.7.1.min.js"></script>
+    <script src="js/bjqs-1.3.min.js"></script>
+    <script src="js/sliderhead.js"></script>
       
 </head>
 <body>
-	<div id="container_main">
+<div id="container_main">
