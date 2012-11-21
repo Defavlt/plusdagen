1a5d3ffaa325298e0c007ab703b100d5aa8ed289
Ashkan Zahiraldini <ashkan.aldini@gmail.com>
2012-11-21 11:01:27 +0100

    andrat utseende pa schema.php. fixat sokvag till bild pa plats.php och fixat med validering pa losenord pa register.php

--- src/php/content/plats.php
+++ src/php/content/plats.php
@@ -8,7 +8,7 @@
 				<div id="karta1">
 				<iframe width="425" height="350" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.se/maps?f=q&amp;source=s_q&amp;hl=sv&amp;geocode=&amp;q=S%C3%B6dra+Malmgatan+7,+Kalmar&amp;aq=0&amp;oq=S%C3%B6dra+M&amp;sll=62.033002,17.378555&amp;sspn=28.471921,107.138672&amp;ie=UTF8&amp;hq=&amp;hnear=S%C3%B6dra+Malmgatan+7,+392+34+Kalmar,+Kalmar+l%C3%A4n&amp;t=m&amp;z=14&amp;ll=56.666295,16.35505&amp;output=embed"></iframe><br /><small><a href="https://maps.google.se/maps?f=q&amp;source=embed&amp;hl=sv&amp;geocode=&amp;q=S%C3%B6dra+Malmgatan+7,+Kalmar&amp;aq=0&amp;oq=S%C3%B6dra+M&amp;sll=62.033002,17.378555&amp;sspn=28.471921,107.138672&amp;ie=UTF8&amp;hq=&amp;hnear=S%C3%B6dra+Malmgatan+7,+392+34+Kalmar,+Kalmar+l%C3%A4n&amp;t=m&amp;z=14&amp;ll=56.666295,16.35505" style="color:#0000FF;text-align:left">Visa större karta</a></small>
 				<br/>
-				<img src="parkering.png" alt="parkeringsplatser" width="425" height="350"/>
+				<img src="../img/parkering.png" alt="parkeringsplatser" width="425" height="350"/>
 				</div>
 				<div id="plats_info">
 				<p>Arbetsmarknadsdagen kommer äga rum på Södra Malmgatan 7. På bilden under visas närliggande parkeringsplatser.</p>
--- src/php/content/schema.php
+++ src/php/content/schema.php
@@ -5,49 +5,22 @@
 			</div>
 			<div id="vad">
 			<h1>Schema för arbetsmarknadsdagen</h1>
-				<table border="6" width="500">
-				<tr>
-				<th>Företag</th>
-				<th>Seminarie</th>
-				<th>Dag</th>
-				<th>Tid</th>
-				</tr>
-
-				<tr>
-				<td>Företag 1</td>
-				<td>Affärside</td>
-				<td>24/5-2013</td>
-				<td>kl:9.00</td>
-				</tr>
-
-				<tr>
-				<td>Företag 2</td>
-				<td>Varför jobba inom it?</td>
-				<td>24/5-2013</td>
-				<td>kl:11.00</td>
-				</tr>
-
-				<tr>
-				<td>Företag 3</td>
-				<td>All tid i världen</td>
-				<td>24/5-2013</td>
-				<td>kl:13.00</td>
-				</tr>
-
-				<tr>
-				<td>Företag 4</td>
-				<td>Framtiden</td>
-				<td>24/5-2013</td>
-				<td>kl.15.00</td>
-				</tr>
-
-				<tr>
-				<td>Företag 5</td>
-				<td>Din tid</td>
-				<td>24/5-2013</td>
-				<td>kl.17.00</td>
-				</tr>
-				</table>
-
+			<p>Här nedan ser ni tiderna för de olika seminarier som är inplanerade.</p>
+			<article>
+			<h2>Bill Gates - Microsoft 10:00</h2>
+			<p>Bill Gates kommer att prata om hur ni som elever skall tänka och agera för att lyckas inom mjukvaruindustrin.</p>
+			</article>
+			<article>
+			<h2>Mark Zuckerberg - Facebook 11:00</h2>
+			<p>Mark Zuckerberg skall berätta sin vision om framtiden inom socialamedia.</p>
+			</article>
+			<article>
+			<h2>Jeff Atwood - Stackexchange 12:00</h2>
+			<p>Jeff Atwood vill ägna sin tid med att berätta om programmeringsyrket och dela med sig av sin erfarnhet.</p>
+			</article>
+			<article>
+			<h2>Steve Jobs - Apple 14:00</h2>
+			<p>Steve Jobs har återuppstått från de döda för att ge Kalmars blivande IT-folk råd om hur de ska föra vidare hans arv.</p>
+			</article>
 			</div>
 	</div> <!-- mainbox slut -->
