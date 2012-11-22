8ab86616cce4568426e3156dd8206df96cb96e17
rikard <rikard.bergqvist@live.com>
2012-11-22 09:52:22 +0100

    jag andrade sa html koden validerades(ja la till alt till bilderna bara)

--- src/php/modules/slide.php
+++ src/php/modules/slide.php
@@ -5,10 +5,10 @@
 
 		<!-- start Basic Jquery Slider -->
 		<ul class="bjqs">
-			<li><img src="img/massa_2.png" title="Bild1"></li>
-			<li><img src="img/hand.png" title="Bild2"></li>
-			<li><img src="img/kalmarslott.png" title="Bild3"></li>
-			<li><img src="img/nyheter.png" title="Bild4"></li>
+			<li><img src="img/massa_2.png" title="Bild1" alt="Mässan"></li>
+			<li><img src="img/hand.png" title="Bild2" alt="Företag"></li>
+			<li><img src="img/kalmarslott.png" title="Bild3" alt="Omgivning"></li>
+			<li><img src="img/nyheter.png" title="Bild4" alt="Nyheter"></li>
 		</ul>
 		<!-- end Basic jQuery Slider -->
 		
