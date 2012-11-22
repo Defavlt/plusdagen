c0c081b7ffa8b67da61f10fee459f32af8262519
Ashkan Zahiraldini <ashkan.aldini@gmail.com>
2012-11-22 11:47:38 +0100

    innehall i schema har uppdaterats

--- src/php/content/schema.php
+++ src/php/content/schema.php
@@ -1,15 +1,14 @@
 ﻿	
 		<div id="mainbox"> <!-- 3 kolumnerna ligger här -->
 			<div id="vad">
-			<h1>Företag</h1>
-				<p>
-				Företag
-				Seminarie
-				Dag
-				Tid
-				</p>
-
-			<h1>Nyheter</h1>
-				<p> Vi har en ny design på sidan</p>
+			<h1>Schema för seminarier</h1>
+			<h2>Coca-Cola 10:00</h2>
+				<p>Emma Larsson berättar om möjligheter och utmaningar som finns inom livsmedelsbranchen och hur IT spelar en kritisk roll i detta.</p>
+			<h2>Apple 12:00</h2>
+				<p>Anders Österström kommer att gå igenom vad Apple vill se hos sina framtida programmerare. Tid kommer att finnas till frågor i slutet på seminariet.</p>
+			<h2>Spotify 14:00</h2>
+			<p>Information om hur spotify är uppbygt och drivet kommer presenteras tillsammans med framtida visioner. Mike Smith håller i seminariet</p>
+			<h2>Pepsi 16:00</h2>
+			<p>Pepsis historia och hinder kommer visas samt lösningar som företaget tagit fram genom åren. Anna Hermansson och Bertil Svan håller seminariet.</p>
 			</div>
-</div>
+		</div>
